00f80e408382696f487cad35e49f91ebc01ef955
Jayden Banks <jayden.banks21@gmail.com>
2021-07-13 13:27:59 -0600

    fixed case question

--- lab.js
+++ lab.js
@@ -231,20 +231,22 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 //Code Here //Done 
 switch (letterGrade) {
   case "A":
-    console.log("The student is doing excellently.");
+    console.log("The student is doing excellently.")
     break
   case "B":
-    console.log("The student is doing well.");
+    console.log("The student is doing well.")
     break
   case "C":
-    console.log("The student is doing alright.");
+    console.log("The student is doing alright.")
     break
   case "D":
-    console.log("The student is not doing very well.");
+    console.log("The student is not doing very well.")
     break
   case "F":
-    console.log("The student is failing.");
+    console.log("The student is failing.")
     break
+  default:
+  console.log('Not an eligible grade.')
 }
 
 
